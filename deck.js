ca48db5fa908364867b3d33c3ba17a7cf67085d8
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-02-19 22:21:01 -0600

    halfway through

---
+++ deck.js
@@ -0,0 +1,52 @@
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+]
+
+export default class Deck {
+    constructor(cards = freshDeck()) {
+        this.cards = cards
+    }
+
+    get numberOfCards() {
+        return this.cards.length
+    }
+
+    shuffle() {
+        // not-great method: this.cards.sort((a,b) => Math.random() - .5)
+        for (let i = this.numberOfCards - 1; i > 0; i--) {
+            const newIndex = Math.floor(Math.random() * (i + 1));
+            const oldValue = this.cards[newIndex];
+            this.cards[newIndex] = this.cards[i];
+            this.cards[i] = oldValue;
+        }
+    }
+}
+
+class Card {
+    constructor(suit, value) {
+        this.suit = suit
+        this.value = value
+    }
+}
+
+function freshDeck() {
+    // flatMap combines various arrays into a single array to be mapped
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit, value)
+        })
+    })
+}
