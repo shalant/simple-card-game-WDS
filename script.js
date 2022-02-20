71e48428568d9f357d45ef077c82d070648dc1f8
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-02-19 22:45:57 -0600

    2827

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 import Deck from './deck.js';
 
+const computerCardSlot = document.querySelector('.computer-card-slot')
+
 const deck = new Deck();
 deck.shuffle();
-console.log(deck.cards);
+
+computerCardSlot.appendChild(deck.cards[0].getHTML())
